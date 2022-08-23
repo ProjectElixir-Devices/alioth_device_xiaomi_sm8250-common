56e1e222bee563f842f538d82ef9cc3a0efcc76a
improbir <ghosh_probir@yahoo.com>
2022-08-23 16:13:34 +0000

    sm8250-common: Drop android.hardware.tetheroffload.config manifest
    * Fixes Wifi tethering issue on Android 13
    
    Signed-off-by: improbir <ghosh_probir@yahoo.com>

--- kona.mk
+++ kona.mk
@@ -588,7 +588,6 @@ PRODUCT_PACKAGES += \
     libwifi-hal-qcom \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
-    android.hardware.tetheroffload.config@1.0 \
     WifiResCommon \
     wpa_supplicant \
     wpa_supplicant.conf
