7a32bcd15a36d5d21d299fd55ada9152f75bec13
johnmart19 <johnivan19999@gmail.com>
2023-02-26 00:26:12 +0200

    sm8250-common: Remove xiaomi-libperfmgr compilation
    This commit reverts:
    5f2f735
    1e96a60
    7386edc
    90cfe7c

--- kona.mk
+++ kona.mk
@@ -434,12 +434,7 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power-service.xiaomi-sm8250-libperfmgr
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+$(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
 
 # Public libraries
 PRODUCT_COPY_FILES += \
